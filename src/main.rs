e5101cb110fe2e4435108f5ac309a860dac3cbe6
klangner <klangner@gmail.com>
2017-02-08 20:13:06 +0100

    led example

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-#![deny(unsafe_code)]
-#![no_main]
 #![no_std]
+#![no_main]
 
 extern crate pg;
 
-#[inline(never)]
 #[no_mangle]
 pub fn main() -> ! {
+    let y;
+    let x = 42;
+    y = x;
+
     loop {}
 }
